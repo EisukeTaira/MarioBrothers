c77da2c0f876eb574783a92cf22b3a3a7a6b372e
BASANAI\i-mekaru <i-mekaru@kbcgroup.ac.jp>
2021-07-26 17:18:31 +0900

    WinMain関数の警告(C28251)回避ための注釈設定追加

--- SBM/SuperMarioBrothers/main.cpp
+++ SBM/SuperMarioBrothers/main.cpp
@@ -1,7 +1,7 @@
 #include "DxLib.h"
 #include "SceneManager.h"
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 	ChangeWindowMode(TRUE);
 
