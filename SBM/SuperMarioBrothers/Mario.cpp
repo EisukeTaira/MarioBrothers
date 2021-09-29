0eb95d4c82a50d9f5e424213371470ad4f99805a
BASANAI\i-mekaru <i-mekaru@kbcgroup.ac.jp>
2021-09-29 16:42:40 +0900

    移動、ダッシュ移動、ターン、ジャンプ作成

--- SBM/SuperMarioBrothers/Mario.cpp
+++ SBM/SuperMarioBrothers/Mario.cpp
@@ -1,188 +1,452 @@
 #include "DxLib.h"
 #include "KeyControl.h"
 #include "Mario.h"
-#define SUCCESS__ 1	// 成功
-#define ERROR__ -1	// 失敗
-#define MAX_SPEED__ 5	// 速度
-#define MARIO_IMAGE_MAX__ 9	//画像の分割数
 
-int images[MARIO_IMAGE_MAX__];
-bool leftFlag;
+// プレイヤー変数
+T_MARIO mario;
 
-int Init(T_MARIO&);
-void Move(T_MARIO&);
-void Jump(T_MARIO&);
-void changeStatus(T_MARIO&, E_MARIO_STATUS);
+// 関数のプロトタイプ宣言
+void Input();
+void Move();
+void Jump();
+int getImageNum();
+void updateDirection();
+
+/******************************************
+* 左辺のフラグに右辺のフラグを追加
+*
+* 引数：	leftFlag 左辺のフラグ
+*			rightFlag 右辺のフラグ
+* 戻り値：	なし
+******************************************/
+inline void addFlag(unsigned char& leftFlag, unsigned char rightFlag) {
+	leftFlag |= rightFlag;
+}
+
+/******************************************
+* 左辺のフラグから右辺のフラグを削除
+*
+* 引数：	leftFlag 左辺のフラグ
+*			rightFlag 右辺のフラグ
+* 戻り値：	なし
+******************************************/
+inline void subFlag(unsigned char& leftFlag, unsigned char rightFlag) {
+	leftFlag &= ~rightFlag;
+}
+
+/******************************************
+* 左辺のフラグに右辺のフラグが含まれているかどうか
+*
+* 引数：	leftFlag 左辺のフラグ
+*			rightFlag 右辺のフラグ
+* 戻り値：	TRUE 含まれている
+*			FALSE 含まれていない
+******************************************/
+inline bool containFlag(unsigned char leftFlag, unsigned char rightFlag) {
+	return leftFlag & rightFlag;
+}
+
+/******************************************
+* 左辺のフラグと右辺のフラグが完全一致
+*
+* 引数：	leftFlag 左辺のフラグ
+*			rightFlag 右辺のフラグ
+* 戻り値：	TRUE 完全一致
+*			FALSE 完全一致以外
+******************************************/
+inline bool equalFlag(unsigned char leftFlag, unsigned char rightFlag) {
+	return leftFlag == rightFlag;
+}
+
+/******************************************
+* マリオの死亡フラグをONにする
+*
+* 引数：	なし
+* 戻り値：	なし
+******************************************/
+inline void marioDie() {
+	mario.isDeath = true;
+}
+
+/******************************************
+* マリオの死亡フラグを取得する
+*
+* 引数：	なし
+* 戻り値：	マリオの死亡フラグ
+******************************************/
+inline const bool getMarioDeathFlag() {
+	return mario.isDeath;
+}
+
+/******************************************
+* マリオのステージクリアフラグを取得する
+*
+* 引数：	なし
+* 戻り値：	マリオのステージクリアフラグ
+******************************************/
+inline const bool getMarioStageClearFlag() {
+	return mario.isStageClear;
+}
+
+/******************************************
+* マリオのダッシュフラグをオンにする
+*
+* 引数：	なし
+* 戻り値：	なし
+******************************************/
+inline void setDashFlagOn() {
+	addFlag(mario.moveFlag, DASH__);
+	mario.maxSpeed.x = MAX_DASH_SPEED__;
+	mario.jumpPower = DASH_JUMP_POWER__;
+}
+
+/******************************************
+* マリオのダッシュフラグをオフにする
+*
+* 引数：	なし
+* 戻り値：	なし
+******************************************/
+inline void setDashFlagOff() {
+	subFlag(mario.moveFlag, DASH__);
+	mario.maxSpeed.x = MAX_WALK_SPEED__;
+	mario.jumpPower = JUMP_POWER__;
+}
+
+/******************************************
+* マリオがジャンプ中かどうか取得する
+*
+* 引数：	なし
+* 戻り値：	TRUE(ジャンプ中)、FALSE(地面にいる)
+******************************************/
+inline bool isJump() {
+	return (mario.oldPosition.y != mario.nowPosition.y);
+}
 
 /******************************************
 * T_MARIO の初期化メソッド
 *
-* 引数：T_MARIO　のインスタンス
-* 戻り値：TRUE(正常終了)、FALSE(異常終了)
+* 引数：	なし
+* 戻り値：	TRUE(正常終了)、FALSE(異常終了)
 ******************************************/
-int Init(T_MARIO& mario) {
-	if (LoadDivGraph("images/mario.png", MARIO_IMAGE_MAX__, MARIO_IMAGE_MAX__, 1, MARIO_WIDTH_HEIGHT__, MARIO_WIDTH_HEIGHT__, images) == ERROR__) {
-		return ERROR__;
+int marioInit() {
+	// TODO : マリオテスト用
+	KeyCtrl_Init();
+
+	// マリオの画像読み込み
+	if(mario.images[0] == 0) {
+		if (LoadDivGraph("images/mario.png", MARIO_IMAGE_MAX__, MARIO_IMAGE_MAX__, 1, MARIO_WIDTH_HEIGHT__, MARIO_WIDTH_HEIGHT__, mario.images) == ERROR__) {
+			// 読み込み失敗
+			return ERROR__;
+		}
 	}
 
-	mario.x = 0;
-	mario.y = 200;
-	mario.speed = 0;
-	mario.acc = 1 / (60 * 1);
-	mario.status = IDOL;
+	// 初期座標
+	mario.nowPosition.x = 0;	// 現在座標 X
+	mario.nowPosition.y = 200;	// 現在座標 Y
+	mario.oldPosition.x = mario.nowPosition.x;	// 1フレーム前座標 X
+	mario.oldPosition.y = mario.nowPosition.y;	// 1フレーム前座標 Y
+
+	mario.speed.x = 0;	// X座標の移動スピード
+	mario.speed.y = 0;	// Y座標の移動スピード
+	mario.maxSpeed.x = 5;	// X座標の最大スピード
+	mario.maxSpeed.y = 8;	// Y座標の最大スピード
+
+	mario.acc.x = 0.2f;	// 加速度X
+	mario.acc.y = 1.0f;	// 加速度Y
+	mario.jumpPower = JUMP_POWER__;
+
+	mario.moveFlag = IDOL__;	// 現在のモード
+	mario.direction = RIGHT_DIR;	// 向き
+
+	// 各フラグはオフ
+	mario.isJumpPush = false;	// ジャンプボタンが押されているか
+	mario.isDeath = false;	// 死亡フラグ
+	mario.isStageClear = false;	// ステージクリアフラグ
+
+	mario.animationFreamTime = 0;	// アニメーション用フレームカウント
+	
+
 	return SUCCESS__;
 }
 
-bool isInitSuccess(T_MARIO& mario) {
-	KeyCtrl_Init();
+/******************************************
+* T_MARIO の更新メソッド
+*
+* 引数：	なし
+* 戻り値：	なし
+******************************************/
+void Update() {
 
-	return Init(mario) == SUCCESS__;
-}
-
-void Update(T_MARIO& mario) {
 	KeyCtrl_Update();
 	
-	switch (mario.status) {
-	case IDOL:
-	case MOVE:
-	case JUMP:
-		Move(mario);
-		Jump(mario);
-		break;
-
-	case GAME_OVER:
-		break;
-
-	case GAME_CLEAR:
-		break;
-	}
-}
-
-void Draw(T_MARIO& mario) {
-	static int animspeed = 4;
-	static int animFreamCount = 0;
-
-	
-	switch (mario.status) {
-	case MOVE:
-		DrawRotaGraph(mario.x + (MARIO_WIDTH_HEIGHT__ / 2), mario.y + (MARIO_WIDTH_HEIGHT__ / 2), 1.0, 0.0, images[animFreamCount / animspeed % 3 + mario.status], TRUE, (mario.direction == LEFT));
-		animFreamCount++;
-		break;
-
-	case GAME_CLEAR:
-		DrawRotaGraph(mario.x + (MARIO_WIDTH_HEIGHT__ / 2), mario.y + (MARIO_WIDTH_HEIGHT__ / 2), 1.0, 0.0, images[animFreamCount / animspeed % 2 + mario.status], TRUE, (mario.direction == LEFT));
-		animFreamCount++;
-		break;
-
-	default:
-		animFreamCount = 0;
-		DrawRotaGraph(mario.x + (MARIO_WIDTH_HEIGHT__ / 2), mario.y + (MARIO_WIDTH_HEIGHT__ / 2), 1.0, 0.0, images[mario.status], TRUE, (mario.direction == LEFT));
-
-	}
-	
-
-	
-}
-
-void Input(T_MARIO& mario) {
-	if (Get_Key().now_key & PAD_INPUT_LEFT || CheckHitKey(KEY_INPUT_A)) {
-		mario.status = MOVE;
-		mario.direction = LEFT;
-		if (0 < mario.acc) {
-			mario.acc = -mario.acc;
+	if (!getMarioDeathFlag()) {
+		if (!getMarioStageClearFlag()) {
+			Input();
+			Move();
+			Jump();
 		}
-	} else if (Get_Key().now_key & PAD_INPUT_RIGHT || CheckHitKey(KEY_INPUT_D)) {
-		mario.status = MOVE;	// ステータス変更
-		mario.direction = RIGHT;	// 方向決定
-		if (mario.acc < 0) {
-			mario.acc = -mario.acc;
-		}
-	} else {
-		if(!(Get_Key().old_key & Get_Key().now_key)){
-			if (mario.speed < 0 && mario.acc < 0) {
-				mario.acc = -mario.acc;
-			} else if(0 < mario.speed && 0 < mario.acc) {
-				mario.acc = -mario.acc;
-			}
-		}
-		if (mario.status != IDOL && mario.status != JUMP) {
-			if (mario.speed == 0.0f) {
-				mario.status = IDOL;
-			} else {
-				mario.speed += mario.acc;
-			}
+		else {
+			// ステージクリア処理
 		}
 	}
-
-	if (Get_Key().key_flg & PAD_INPUT_A || CheckHitKey(KEY_INPUT_SPACE)) {
-		mario.status = JUMP;
+	else {
+		// 死亡処理
 	}
 }
 
 /******************************************
-* T_MARIO の移動
+* T_MARIO の描画メソッド
 *
-* 引数：T_MARIO　のインスタンス
-* 戻り値：TRUE(正常終了)、FALSE(異常終了)
+* 引数：	なし
+* 戻り値：	なし
 ******************************************/
-void Move(T_MARIO& mario) {
-	if (mario.direction == RIGHT) {
-		if (mario.status != TURN) {
-			// 左へ進んでた時、statusをTURNにする
-			if(mario.speed < 0.0f) {
-				mario.status = TURN;
-			}
-		} else {
-			if (0.0f <= mario.speed) {
-				// 右へ進み始めたらstatusをTURNからMOVEに変更
-				mario.status = MOVE;
-			}
-		}
-	} else {
-		if (mario.status != TURN) {
-			// 右へ進んでた時、statusをTURNにする
-			if (0.0f < mario.speed) {
-				mario.status = TURN;
-			}
-		}
-		else {
-			if (mario.speed <= 0.0f) {
-				// 左へ進み始めたらstatusをTURNからMOVEに変更
-				mario.status = MOVE;
-			}
-		}
-	}
+void Draw() {
+	// 左右の向き決定
+	updateDirection();
 
-	mario.speed += mario.acc;
-	if (0 < mario.speed) {
-		if (1.0f < mario.speed) {
-			mario.speed = 1.0f;
-		}
-	}
-	else {
-		if (mario.speed < -1.0f) {
-			mario.speed = -1.0f;
-		}
-	}
+
+	// 表示画像の決定
+	int displayImageNum = getImageNum();
+		//mario.imageNum;
+	//if (mario.moveFlag & MOVE__ && (~mario.moveFlag & JUMP__) && (~mario.moveFlag & TURN__)) {
+	//	// MOVE__の時だけアニメーションにする
+	//	displayImageNum += mario.animationWalkAddNum;
+	//}
+
+	// 描画座標 X
+	int x = (int)mario.nowPosition.x + (MARIO_WIDTH_HEIGHT__ / 2);
+	// 描画座標 Y
+	int y = (int)mario.nowPosition.y + (MARIO_WIDTH_HEIGHT__ / 2);
+	// 向き (左 = TRUE, 右 = FALSE)
+	bool isLeftDir = (mario.direction == LEFT_DIR);
+
+	// マリオを描画
+	DrawRotaGraph(x, y, 1.0, 0.0, mario.images[displayImageNum], TRUE, isLeftDir);
+
 }
 
-void Jump(T_MARIO& mario) {
-	static int jumpFreamCount = 0;
+/******************************************
+* マリオの入力判定メソッド
+*
+* 引数：	なし
+* 戻り値：	なし
+******************************************/
+void Input() {
+	if (Get_Key().key_flg & PAD_INPUT_B) {	// ダッシュフラグをオン
+		setDashFlagOn();
+
+	} else if (containFlag(mario.moveFlag, DASH__) && !(Get_Key().now_key & PAD_INPUT_B)) {	// ダッシュフラグをオフ
+		setDashFlagOff();
+	}
 	
-  	if (mario.status == JUMP || (Get_Key().key_flg & PAD_INPUT_A) || CheckHitKey(KEY_INPUT_SPACE)) {
-		if (jumpFreamCount++ < 15) {
-			mario.y -= MAX_SPEED__;
+	// 左キーが押されている
+	if(Get_Key().now_key & PAD_INPUT_LEFT) {
+
+		// 左キーを押した瞬間
+		if (Get_Key().key_flg & PAD_INPUT_LEFT) {
+			addFlag(mario.moveFlag, MOVE__);
+			subFlag(mario.moveFlag, TURN__);
+
+			mario.animationFreamTime = 0;
+
+			// スピードが右方向の時ターンさせる
+			if (1.0f <= mario.speed.x && !isJump()) {
+				addFlag(mario.moveFlag, TURN__);
+			}
+
+		}	// ターンの時に歩く画像に変更
+		else if(containFlag(mario.moveFlag, TURN__) && mario.speed.x <= 0.5f){
+			subFlag(mario.moveFlag, TURN__);
+			mario.speed.x = 0;
+		}
+
+		// 左へ移動
+		mario.speed.x -= mario.acc.x;
+		if (mario.speed.x < -mario.maxSpeed.x) {
+			mario.speed.x = -mario.maxSpeed.x;
+		}
+
+	}	// 右キーが押されている
+	else if (Get_Key().now_key & PAD_INPUT_RIGHT) {
+		
+		// 右キーが押された瞬間
+		if (Get_Key().key_flg & PAD_INPUT_RIGHT) {
+			addFlag(mario.moveFlag, MOVE__);
+			subFlag(mario.moveFlag, TURN__);
+
+			mario.animationFreamTime = 0;
+
+			// スピードが左方向の時ターンさせる
+			if (mario.speed.x  <= -1.0f && !isJump()) {
+				addFlag(mario.moveFlag, TURN__);
+			}
+
+		}	// ターンの時に歩く画像に変更
+		else if (containFlag(mario.moveFlag, TURN__) && -0.5f <= mario.speed.x) {
+			subFlag(mario.moveFlag, TURN__);
+			mario.speed.x = 0;
+		}
+
+		// 右へ移動
+		mario.speed.x += mario.acc.x;
+		if (mario.maxSpeed.x < mario.speed.x) {
+			mario.speed.x = mario.maxSpeed.x;
+		}
+
+	}	// 移動キーが押されていない
+	else {
+		subFlag(mario.moveFlag, MOVE__);
+	}
+
+	
+	// ジャンプボタンが押された瞬間にジャンプフラグON
+	if (!isJump() && Get_Key().key_flg & PAD_INPUT_A) {
+		subFlag(mario.moveFlag, TURN__);
+		addFlag(mario.moveFlag, JUMP__);
+		mario.isJumpPush = true;
+		mario.speed.y = -mario.jumpPower;
+
+	}	// ジャンプボタンが押していないフラグに変更
+	else  if (mario.isJumpPush && !(Get_Key().now_key & PAD_INPUT_A)) {
+		mario.isJumpPush = false;
+	}
+}
+
+/******************************************
+* マリオの横移動
+*
+* 引数：	なし
+* 戻り値：	なし
+******************************************/
+void Move() {
+	const float STOP_SPEED__ = 0.3f;
+	if(equalFlag(mario.moveFlag, IDOL__) || (!containFlag(mario.moveFlag, MOVE__) && mario.speed.x != 0)) {
+		// 横に移動している場合は徐々に停止する
+		if (mario.speed.x != 0) {
+
+			// 右移動中
+			if (0 < mario.speed.x) {
+				mario.speed.x -= mario.acc.x;
+				if (mario.speed.x <= STOP_SPEED__) {
+					mario.speed.x = 0;
+				}
+
+			}	// 左移動中
+			else {
+				mario.speed.x += mario.acc.x;
+				if (-STOP_SPEED__ <= mario.speed.x) {
+					mario.speed.x = 0;
+				}
+			}
+		}
+	}
+
+	// 現在座標Ｘの保存
+	mario.oldPosition.x = mario.nowPosition.x;
+	// 現在座標Ｘの更新
+	mario.nowPosition.x += mario.speed.x;
+}
+
+/******************************************
+* マリオのジャンプと落下移動
+*
+* 引数：	なし
+* 戻り値：	なし
+******************************************/
+void Jump() {
+	if (isJump() || containFlag(mario.moveFlag, JUMP__)) {
+
+		// ジャンプフラグをONにする
+		if (!containFlag(mario.moveFlag, JUMP__)) {
+			addFlag(mario.moveFlag, JUMP__);
+		}
+
+		// 重力でジャンプスピードを減衰
+		if (mario.speed.y < 0 && mario.isJumpPush) {
+			mario.speed.y += (mario.acc.y / 2.0);
 		}
 		else {
-			mario.y += MAX_SPEED__;
+			mario.speed.y += mario.acc.y;
+		}
 
-			// ジャンプ終了を時間で制御。
-			// 本来は地面についたらジャンプ終了
-			if (jumpFreamCount == 30) {
-				jumpFreamCount = 0;
-				mario.status = IDOL;
-			}
+		// 落下がmaxSpeed以上にならない
+		if (mario.maxSpeed.y < mario.speed.y) {
+			mario.speed.y = mario.maxSpeed.y;
+		}
+
+		// 現在座標Ｙの保存
+		mario.oldPosition.y = mario.nowPosition.y;
+		// 現在座標Ｙの更新
+		mario.nowPosition.y += mario.speed.y;
+
+		// 着地処理
+		if (mario.nowPosition.y > 200) {
+			// めり込まない座標に修正
+			mario.nowPosition.y = 200;
+			
+			// 移動を停止する
+			mario.speed.y = 0;
+
+			// フラグをオフにする
+			subFlag(mario.moveFlag, JUMP__);
+			mario.isJumpPush = false;
 		}
 	}
 }
 
+/******************************************
+* マリオの向いている方向を更新する
+*
+* 引数：	なし
+* 戻り値：	なし
+******************************************/
+void updateDirection() {
+	if (mario.speed.x != 0) {
+		if (mario.speed.x < 0) {
+			mario.direction = LEFT_DIR;
+
+		}
+		else {
+			mario.direction = RIGHT_DIR;
+		}
+	}
+}
+
+/******************************************
+* マリオの表示画像の配列番号を取得
+*
+* 引数：	なし
+* 戻り値：	なし
+******************************************/
+int getImageNum() {
+	if (equalFlag(mario.moveFlag, IDOL__)) {
+		return IDOL_IMG__;
+	}
+
+	if (containFlag(mario.moveFlag, TURN__)) {
+		return TURN_IMG__;
+	}
+
+	if (containFlag(mario.moveFlag, JUMP__)) {
+		return JUMP_IMG__;
+	}
+
+	if (containFlag(mario.moveFlag, MOVE__)) {
+		mario.animationFreamTime++;
+		int freamNum = 5;	// 連続で表示するフレーム数
+		if (containFlag(mario.moveFlag, DASH__)) {
+			freamNum = 3;
+		}
+		return MOVE_IMG_START__ + mario.animationFreamTime % ((MOVE_IMG_END__ - MOVE_IMG_START__ + 1) * freamNum) / freamNum;
+	}
+}
+
+/******************************************
+* プレイヤー情報取得
+*
+* 引数：	なし
+* 戻り値：	定数のプレイヤー情報
+******************************************/
+const T_MARIO getMarioInfo() {
+	return mario;
+}
--- SBM/SuperMarioBrothers/Mario.h
+++ SBM/SuperMarioBrothers/Mario.h
@@ -1,31 +1,77 @@
 #pragma once
-#define MARIO_WIDTH_HEIGHT__ 32
 
-typedef enum {
-	IDOL = 0,
-	MOVE,
-	MOVE_END = 3,
-	TURN,
-	JUMP,
-	GAME_OVER,
-	GAME_CLEAR,
-	GAME_CLEAR_END
-}E_MARIO_STATUS;
+// 画像系定数
+#define MARIO_WIDTH_HEIGHT__ 32	// 画像の幅
+#define MARIO_IMAGE_MAX__ 9		// 画像の分割数
 
+// 画像の添え字
+#define IDOL_IMG__ 0			// 棒立ち
+#define MOVE_IMG_START__ 1		// 最初の移動画像
+#define MOVE_IMG_END__ 3		// 最後の移動画像
+#define TURN_IMG__ 4			// ターン
+#define JUMP_IMG__ 5			// ジャンプ
+
+// 移動状態を表す定数
+#define IDOL__ 0x00U			// 棒立ち
+#define MOVE__ 0x01U			// 移動
+#define DASH__ 0x02U			// ダッシュ
+#define TURN__ 0x04U			// ターン
+#define JUMP__ 0x08U			// ジャンプ
+
+// ステータス
+#define MAX_WALK_SPEED__ 3		// 通常時の最高速度
+#define MAX_DASH_SPEED__ 5		// ダッシュ時の最高速度
+#define JUMP_POWER__ 10			// 通常時のジャンプ力
+#define DASH_JUMP_POWER__ 13	// ダッシュ時のジャンプ力
+
+// 真偽
+#define SUCCESS__ 1	// 成功
+#define ERROR__ -1	// 失敗
+
+// 方向指定の列挙
 typedef enum {
-	LEFT = -1,
-	RIGHT = 1
+	LEFT_DIR = -1,
+	RIGHT_DIR = 1
 }E_MARIO_DIRECTION;
 
+// 座標の構造体
 typedef struct {
-	int x;	// 座標X
-	int y;	// 座標Y
-	float acc;	// 加速度
-	float speed;	// 速度
-	E_MARIO_DIRECTION direction;
-	E_MARIO_STATUS status;
+	float x;
+	float y;
+}T_COORDINATE;
+
+// マリオの構造体
+typedef struct {
+	T_COORDINATE oldPosition;		// 1フレーム前の座標
+	T_COORDINATE nowPosition;		// 現在座標
+	T_COORDINATE speed;				// 速度　座標に加算する値
+	T_COORDINATE maxSpeed;			// 最大速度　スピードの上限値
+	T_COORDINATE acc;				// 加速度　スピードに加算する値
+	float jumpPower;				// ジャンプ力
+	
+	bool isJumpPush;				// ジャンプボタンを押しているかどうか
+	bool isStageClear;				// ステージクリアのフラグ
+	bool isDeath;					// GameOverのフラグ
+
+	unsigned char moveFlag;		// 移動状態
+	E_MARIO_DIRECTION direction;	// 左右の向き
+
+	int animationFreamTime;			// アニメーション用フレームカウント
+	int images[MARIO_IMAGE_MAX__];	// マリオの画像
+
+	
 }T_MARIO;
 
-bool isInitSuccess(T_MARIO&);
-void Update(T_MARIO&);
-void Draw(T_MARIO&);
+// 初期化メソッド
+inline bool isMarioInitSuccess() {
+	int marioInit();
+	return (marioInit() == SUCCESS__);
+}
+
+// 更新処理
+void Update();
+
+// 描画処理
+void Draw();
+
+const T_MARIO getMarioInfo();
